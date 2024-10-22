3ca18bc3457cb19813120fd325313a3bb3d6b51e
KompelliSravanSyncfusion <kompellisravankumark@syncfusion.com>
2024-10-22 18:18:04 +0530

    FLUT-892072-[others][flutter]: Updated the sample

--- lib/main.dart
+++ lib/main.dart
@@ -9,93 +9,119 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: false),
-      home: _MyHomePage(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: true),
+      home: PieChart(),
     );
   }
 }
 
-class _MyHomePage extends StatefulWidget {
+class PieChart extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _PieChartState createState() => _PieChartState();
 }
 
-class _MyHomePageState extends State<_MyHomePage> {
+class _PieChartState extends State<PieChart> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  late List<_ElectronicData> _electronicProducts;
   late LabelIntersectAction _labelIntersectAction;
-  late List<_ChartSampleData> _electronicProducts;
+  late String _selectedItem;
+
+  List<String>? _labelIntersectActionMode;
 
   @override
   void initState() {
+    _selectedItem = 'shift';
     _electronicProducts = _buildChartData();
     _labelIntersectAction = LabelIntersectAction.shift;
+    _labelIntersectActionMode = <String>['shift', 'none', 'hide'].toList();
     super.initState();
   }
 
-  List<_ChartSampleData> _buildChartData() {
+  List<_ElectronicData> _buildChartData() {
     return [
-      _ChartSampleData('Smartphones', 150),
-      _ChartSampleData('Laptops', 145),
-      _ChartSampleData('Tablets', 140),
-      _ChartSampleData('Smartwatches', 138),
-      _ChartSampleData('Headphones', 135),
-      _ChartSampleData('Smart Home Devices', 133),
-      _ChartSampleData('TVs', 130),
-      _ChartSampleData('Gaming Consoles', 128),
-      _ChartSampleData('Monitors', 125),
-      _ChartSampleData('Printers', 123),
-      _ChartSampleData('Cameras', 120),
-      _ChartSampleData('Routers', 118),
-      _ChartSampleData('External Storage', 115),
-      _ChartSampleData('Projectors', 113),
-      _ChartSampleData('VR Headsets', 110),
-      _ChartSampleData('Microphones', 108),
-      _ChartSampleData('Webcams', 105),
-      _ChartSampleData('Smart Glasses', 103),
-      _ChartSampleData('Fitness Trackers', 100),
-      _ChartSampleData('Bluetooth Speakers', 98),
-      _ChartSampleData('E-readers', 95),
-      _ChartSampleData('Network Switches', 93),
-      _ChartSampleData('Docking Stations', 90),
-      _ChartSampleData('Power Banks', 88),
-      _ChartSampleData('Smart Thermostats', 85),
-      _ChartSampleData('Surge Protectors', 83),
-      _ChartSampleData('Home Security Cameras', 80),
-      _ChartSampleData('Smart Plugs', 78),
-      _ChartSampleData('Electronic Keyboards', 75),
-      _ChartSampleData('Digital Picture Frames', 73),
-      _ChartSampleData('Satellite Radios', 70),
-      _ChartSampleData('Car Dash Cameras', 68),
-      _ChartSampleData('Digital Pianos', 65),
-      _ChartSampleData('Smart Doorbells', 63),
-      _ChartSampleData('Weather Stations', 60),
-      _ChartSampleData('Robotic Vacuums', 58),
-      _ChartSampleData('Home Audio Systems', 55),
+      _ElectronicData('Smartphones', 150),
+      _ElectronicData('Laptops', 145),
+      _ElectronicData('Tablets', 140),
+      _ElectronicData('Smartwatches', 138),
+      _ElectronicData('Headphones', 135),
+      _ElectronicData('Smart Home Devices', 133),
+      _ElectronicData('TVs', 130),
+      _ElectronicData('Gaming Consoles', 128),
+      _ElectronicData('Monitors', 125),
+      _ElectronicData('Printers', 123),
+      _ElectronicData('Cameras', 120),
+      _ElectronicData('Routers', 118),
+      _ElectronicData('External Storage', 115),
+      _ElectronicData('Projectors', 113),
+      _ElectronicData('VR Headsets', 110),
+      _ElectronicData('Microphones', 108),
+      _ElectronicData('Webcams', 105),
+      _ElectronicData('Smart Glasses', 103),
+      _ElectronicData('Fitness Trackers', 100),
+      _ElectronicData('Bluetooth Speakers', 98),
+      _ElectronicData('E-readers', 95),
+      _ElectronicData('Network Switches', 93),
+      _ElectronicData('Docking Stations', 90),
+      _ElectronicData('Power Banks', 88),
+      _ElectronicData('Smart Thermostats', 85),
+      _ElectronicData('Surge Protectors', 83),
+      _ElectronicData('Home Security Cameras', 80),
+      _ElectronicData('Smart Plugs', 78),
+      _ElectronicData('Electronic Keyboards', 75),
+      _ElectronicData('Digital Picture Frames', 73),
+      _ElectronicData('Satellite Radios', 70),
+      _ElectronicData('Car Dash Cameras', 68),
+      _ElectronicData('Digital Pianos', 65),
+      _ElectronicData('Smart Doorbells', 63),
+      _ElectronicData('Weather Stations', 60),
+      _ElectronicData('Robotic Vacuums', 58),
+      _ElectronicData('Home Audio Systems', 55),
     ];
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(
+        title: const Text(''),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () {
+              setState(() {
+                _scaffoldKey.currentState?.openEndDrawer();
+              });
+            },
+          ),
+        ],
+      ),
+      endDrawer: Drawer(
+        child: _buildForm(),
+      ),
       body: Row(
         children: [
           Expanded(
-            child: _buildCircularChartWithLabels(),
+            flex: 3,
+            child: _buildCircularChart(),
           ),
-          _buildLabelControlButtons(),
         ],
       ),
     );
   }
 
-  /// Builds the circular chart with data labels and custom connector line settings.
-  Widget _buildCircularChartWithLabels() {
+  /// Builds the circular chart with data labels and
+  /// custom connector line settings.
+  Widget _buildCircularChart() {
     return SfCircularChart(
       series: <CircularSeries>[
-        PieSeries<_ChartSampleData, String>(
+        PieSeries<_ElectronicData, String>(
           dataSource: _electronicProducts,
-          xValueMapper: (_ChartSampleData data, int index) => data.x,
-          yValueMapper: (_ChartSampleData data, int index) => data.y,
-          dataLabelMapper: (_ChartSampleData data, int index) =>
+          xValueMapper: (_ElectronicData data, int index) => data.x,
+          yValueMapper: (_ElectronicData data, int index) => data.y,
+          dataLabelMapper: (_ElectronicData data, int index) =>
               '${data.x}: ${data.y}',
           dataLabelSettings: DataLabelSettings(
             isVisible: true,
@@ -112,56 +138,72 @@ class _MyHomePageState extends State<_MyHomePage> {
     );
   }
 
-  /// Builds the buttons to control label intersection behavior (Shift, Hide, None).
-  Widget _buildLabelControlButtons() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(50),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+  /// Method to build the form with text and dropdown.
+  Widget _buildForm() {
+    return Padding(
+      padding: const EdgeInsets.all(15),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
             children: [
-              const Text(
-                "LabelIntersectAction",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
+              const Expanded(
+                flex: 7,
+                child: Text(
+                  "Label intersect action",
+                  style: TextStyle(fontWeight: FontWeight.bold),
                 ),
               ),
-              TextButton(
-                onPressed: () {
-                  setState(() {
-                    _labelIntersectAction = LabelIntersectAction.shift;
-                  });
-                },
-                child: const Text('Shift'),
-              ),
-              TextButton(
-                onPressed: () {
-                  setState(() {
-                    _labelIntersectAction = LabelIntersectAction.hide;
-                  });
-                },
-                child: const Text('Hide'),
-              ),
-              TextButton(
-                onPressed: () {
-                  setState(() {
-                    _labelIntersectAction = LabelIntersectAction.none;
-                  });
-                },
-                child: const Text('None'),
+              Expanded(
+                flex: 3,
+                child: Container(
+                  alignment: Alignment.centerRight,
+                  child: DropdownButton<String>(
+                    focusColor: Colors.transparent,
+                    underline: Container(
+                      color: const Color(0xFFBDBDBD),
+                      height: 1,
+                    ),
+                    value: _selectedItem,
+                    items: _labelIntersectActionMode!.map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value,
+                            style: const TextStyle(color: Colors.black)),
+                      );
+                    }).toList(),
+                    onChanged: (dynamic value) {
+                      _updateLabelIntersectAction(value.toString());
+                    },
+                  ),
+                ),
               ),
             ],
           ),
-        ),
-      ],
+        ],
+      ),
     );
+  }
+
+  /// Method to update the change in label intersect action.
+  void _updateLabelIntersectAction(String item) {
+    setState(() {
+      _selectedItem = item;
+      if (_selectedItem == 'shift') {
+        _labelIntersectAction = LabelIntersectAction.shift;
+      }
+      if (_selectedItem == 'none') {
+        _labelIntersectAction = LabelIntersectAction.none;
+      }
+      if (_selectedItem == 'hide') {
+        _labelIntersectAction = LabelIntersectAction.hide;
+      }
+    });
   }
 }
 
-class _ChartSampleData {
-  _ChartSampleData(this.x, this.y);
+class _ElectronicData {
+  _ElectronicData(this.x, this.y);
   String x;
   int y;
 }
